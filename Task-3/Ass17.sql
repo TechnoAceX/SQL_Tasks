7a394557a29b27cb9296a3e517fd99a5b6ad3a60
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2025-06-18 00:18:49 +0530

    Update Ass17.sql

--- Task-3/Ass17.sql
+++ Task-3/Ass17.sql
@@ -6,4 +6,5 @@ CREATE LOGIN intern_user WITH PASSWORD = 'StrongPassword123!';
 -- Step 2: Create User in current database
 CREATE USER intern_user FOR LOGIN intern_user;
 
-
+-- Step 3: Grant db_owner role
+EXEC sp_addrolemember 'db_owner', 'intern_user';
