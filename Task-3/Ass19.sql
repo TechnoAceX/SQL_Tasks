c0d6cebc64ae98faf41e28cff1d8ee257d7d31c4
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2025-06-18 00:35:52 +0530

    Update Ass19.sql

--- Task-3/Ass19.sql
+++ Task-3/Ass19.sql
@@ -1,1 +1,15 @@
+-- Task 19: Average Salary Error with '0' Removed
 
+CREATE TABLE EMPLOYEES (
+    emp_id INT,
+    salary INT
+);
+
+INSERT INTO EMPLOYEES VALUES
+(1, 1000),
+(2, 2000),
+(3, 3000);
+
+-- Query
+SELECT CEIL(AVG(salary) - AVG(CAST(REPLACE(CAST(salary AS VARCHAR), '0', '') AS INT))) AS avg_diff
+FROM EMPLOYEES;
