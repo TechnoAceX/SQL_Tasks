05470346eb0ad3d56cf2eca95b9ad70f5bdaf2df
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2025-06-17 22:38:00 +0530

    Step 1: Create Tables

--- Task-3/Ass4.sql
+++ Task-3/Ass4.sql
@@ -1,1 +1,30 @@
+-- Task4:- Step 1: Create Tables
+CREATE TABLE Contests (
+    contest_id INT,
+    hacker_id INT,
+    name VARCHAR(100)
+);
+
+CREATE TABLE Colleges (
+    college_id INT,
+    contest_id INT
+);
+
+CREATE TABLE Challenges (
+    challenge_id INT,
+    college_id INT
+);
+
+CREATE TABLE View_Stats (
+    challenge_id INT,
+    total_views INT,
+    total_unique_views INT
+);
+
+CREATE TABLE Submission_Stats (
+    challenge_id INT,
+    total_submissions INT,
+    total_accepted_submissions INT
+);
+
 
