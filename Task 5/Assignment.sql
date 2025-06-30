cf25bbea33191d0ae4707f44fbacd285383f32d8
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2025-06-30 16:12:43 +0530

    Tables

--- Task 5/Assignment.sql
+++ Task 5/Assignment.sql
@@ -1,11 +1,32 @@
--- Required Tables
-CREATE TABLE SubjectAllotments (
-    StudentId VARCHAR(20),
-    SubjectId VARCHAR(20),
-    Is_Valid BIT
+-- 1. Create Required Tables
+
+CREATE TABLE StudentDetails (
+    StudentId INT PRIMARY KEY,
+    StudentName VARCHAR(100),
+    GPA DECIMAL(4,2),
+    Branch VARCHAR(10),
+    Section VARCHAR(10)
 );
 
-CREATE TABLE SubjectRequest (
-    StudentId VARCHAR(20),
-    SubjectId VARCHAR(20)
+CREATE TABLE SubjectDetails (
+    SubjectId VARCHAR(10) PRIMARY KEY,
+    SubjectName VARCHAR(100),
+    MaxSeats INT,
+    RemainingSeats INT
+);
+
+CREATE TABLE StudentPreference (
+    StudentId INT,
+    SubjectId VARCHAR(10),
+    Preference INT,
+    PRIMARY KEY (StudentId, Preference)
+);
+
+CREATE TABLE Allotments (
+    SubjectId VARCHAR(10),
+    StudentId INT
+);
+
+CREATE TABLE UnallotedStudents (
+    StudentId INT PRIMARY KEY
 );
