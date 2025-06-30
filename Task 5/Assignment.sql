f589161cfe41e8912eedb74767441bdcc2f689b8
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2025-06-30 16:11:03 +0530

    Create Tables

--- Task 5/Assignment.sql
+++ Task 5/Assignment.sql
@@ -1,1 +1,11 @@
+-- Required Tables
+CREATE TABLE SubjectAllotments (
+    StudentId VARCHAR(20),
+    SubjectId VARCHAR(20),
+    Is_Valid BIT
+);
 
+CREATE TABLE SubjectRequest (
+    StudentId VARCHAR(20),
+    SubjectId VARCHAR(20)
+);
