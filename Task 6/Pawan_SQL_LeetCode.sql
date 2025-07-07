dbee72e9f73161758960abacf8d7425103fbbd06
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2025-07-07 09:58:10 +0530

    Q2

--- Task 6/Pawan_SQL_LeetCode.sql
+++ Task 6/Pawan_SQL_LeetCode.sql
@@ -4,4 +4,9 @@ SELECT product_id
 FROM Products
 WHERE low_fats = 'Y' AND recyclable = 'Y';
 
+-- 2. Question: Customers Who Never Order
+-- https://leetcode.com/problems/customers-who-never-order/
+SELECT name AS Customers
+FROM Customers
+WHERE id NOT IN (SELECT customerId FROM Orders);
 
