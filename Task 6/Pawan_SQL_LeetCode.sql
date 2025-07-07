8cdbf6e4971819d10f695b9d9527e7eb7e8451be
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2025-07-07 10:01:57 +0530

    Q8

--- Task 6/Pawan_SQL_LeetCode.sql
+++ Task 6/Pawan_SQL_LeetCode.sql
@@ -42,3 +42,10 @@ SELECT class
 FROM Courses
 GROUP BY class
 HAVING COUNT(DISTINCT student) >= 5;
+
+-- 8. Question: Employees Earning More Than Their Managers
+-- https://leetcode.com/problems/employees-earning-more-than-their-managers/
+SELECT e.name AS Employee
+FROM Employee e
+JOIN Employee m ON e.managerId = m.id
+WHERE e.salary > m.salary;
