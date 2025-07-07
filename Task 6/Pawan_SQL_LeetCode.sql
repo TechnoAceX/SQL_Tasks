43ac0d712bcd659a1f690b98d020690257896778
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2025-07-07 09:59:20 +0530

    Q4

--- Task 6/Pawan_SQL_LeetCode.sql
+++ Task 6/Pawan_SQL_LeetCode.sql
@@ -16,3 +16,9 @@ SELECT name, bonus
 FROM Employee e
 LEFT JOIN Bonus b ON e.empId = b.empId
 WHERE bonus < 1000 OR bonus IS NULL;
+
+-- 4. Question: Find Customer Referee
+-- https://leetcode.com/problems/find-customer-referee/
+SELECT name
+FROM Customer
+WHERE referee_id != 2 OR referee_id IS NULL;
