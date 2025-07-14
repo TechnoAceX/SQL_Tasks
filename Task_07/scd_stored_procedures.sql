d3d0467085e1d164caa725b60436b8268334c017
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2025-07-14 23:18:54 +0530

    Type 1

--- Task_07/scd_stored_procedures.sql
+++ Task_07/scd_stored_procedures.sql
@@ -9,3 +9,15 @@ BEGIN
 END;
 
 
+-- SCD Type 1 – Overwrite (No history)
+CREATE PROCEDURE scd_type_1()
+BEGIN
+    MERGE INTO dim_customer AS target
+    USING stg_customer AS source
+    ON target.customer_id = source.customer_id
+    WHEN MATCHED THEN
+        UPDATE SET target.email = source.email
+    WHEN NOT MATCHED THEN
+        INSERT (customer_id, email)
+        VALUES (source.customer_id, source.email);
+END;
