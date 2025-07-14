8b579ffb814eadbd88d1db1c948c60d877f5a261
Pawan Manghnani <84667321+TechnoAceX@users.noreply.github.com>
2025-07-14 23:18:32 +0530

    Type 0

--- Task_07/scd_stored_procedures.sql
+++ Task_07/scd_stored_procedures.sql
@@ -1,1 +1,11 @@
+-- SCD Type 0 – Fixed Attributes (No change allowed)
+CREATE PROCEDURE scd_type_0()
+BEGIN
+    INSERT INTO dim_customer (customer_id, email)
+    SELECT s.customer_id, s.email
+    FROM stg_customer s
+    LEFT JOIN dim_customer d ON s.customer_id = d.customer_id
+    WHERE d.customer_id IS NULL;
+END;
+
 
